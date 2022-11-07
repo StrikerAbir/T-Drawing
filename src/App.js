9b2994e5c362f07da5c9e108d8741f5c7894d04d
Abir Hasan <cse.170201063@gmail.com>
2022-11-08 03:54:38 +0600

    footer done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { router } from "./routes/routes";
 
 function App() {
   return (
-    <div className="App">
+    <div className="max-w-screen-xl mx-auto">
       <RouterProvider router={router}></RouterProvider>
       <Toaster position="top-center" reverseOrder={false} />
     </div>
