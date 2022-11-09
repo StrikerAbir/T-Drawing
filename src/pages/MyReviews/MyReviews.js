fb2fdd03abc0b1bd71b8b85a98c4524410d9eb9e
Abir Hasan <cse.170201063@gmail.com>
2022-11-09 20:09:18 +0600

    edited

--- src/pages/MyReviews/MyReviews.js
+++ src/pages/MyReviews/MyReviews.js
@@ -5,67 +5,69 @@ import { AuthContext } from "../../context/AuthProvider";
 import MyReviewsRow from "./MyReviewsRow/MyReviewsRow";
 import toast from "react-hot-toast";
 
-const MyReviews = ({service_id}) => {
+const MyReviews = ({ service_id }) => {
   useTitle("MyReviews");
-    const { user,logOut } = useContext(AuthContext);
-    const [reviews, setReviews] = useState([]);
-    // console.log(`http://localhost:1000/reviews?email=${user.email}`);
-    useEffect(() => {
-      fetch(`https://t-drawing-server.vercel.app/reviews?email=${user.email}`, {
+  const { user, logOut } = useContext(AuthContext);
+  const [reviews, setReviews] = useState([]);
+  // console.log(`http://localhost:1000/reviews?email=${user.email}`);
+  useEffect(() => {
+    fetch(`https://t-drawing-server.vercel.app/reviews?email=${user.email}`, {
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    })
+      .then((res) => {
+        if (res.status === 401 || res.status === 403) {
+          return logOut();
+        }
+        return res.json();
+      })
+      .then((data) => setReviews(data));
+  }, [user?.email, logOut]);
+
+  const handleDelete = (id) => {
+    const proceed = window.confirm(
+      "Are you sure? You want to remove feedback.?"
+    );
+    if (proceed) {
+      fetch(`https://t-drawing-server.vercel.app/reviews/${id}`, {
+        method: "DELETE",
         headers: {
           authorization: `Bearer ${localStorage.getItem("token")}`,
         },
       })
-        .then((res) => {
-          if (res.status === 401 || res.status === 403) {
-            return logOut();
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.deletedCount > 0) {
+            toast.success("Feedback Removed Successfully");
+            const remaining = reviews.filter((review) => review._id !== id);
+            setReviews(remaining);
           }
-          return res.json();
-        })
-        .then((data) => setReviews(data));
-    }, [user?.email, logOut]);
-
-    const handleDelete = (id) => {
-      const proceed = window.confirm("Are you sure? You want to remove feedback.?");
-      if (proceed) {
-        fetch(`https://t-drawing-server.vercel.app/reviews/${id}`, {
-          method: "DELETE",
-          headers: {
-            authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
-        })
-          .then((res) => res.json())
-          .then((data) => {
-            if (data.deletedCount > 0) {
-              toast.success("Feedback Removed Successfully");
-              const remaining = reviews.filter((review) => review._id !== id);
-              setReviews(remaining);
-            }
-          });
-      }
-    };
-
-    const handleUpdate = (object) => {
-        const { _id } = object;
-        console.log(object);
-        console.log(`http://localhost:1000/reviews/${_id}`);
-        fetch(`http://localhost:1000/reviews/${_id}`, {
-          method: "PATCH",
-          headers: {
-            "content-type": "application/json",
-            authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
-          body: JSON.stringify(object),
-        })
-          .then((res) => res.json())
-          .then((data) => {
-            console.log(data);
-            if (data.modifiedCount > 0) {
-                toast.success("Feedback Updated Successfully");
-                window.location.reload();
-            }
-          });
+        });
     }
+  };
+
+  const handleUpdate = (object) => {
+    const { _id } = object;
+    console.log(object);
+    console.log(`http://localhost:1000/reviews/${_id}`);
+    fetch(`http://localhost:1000/reviews/${_id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      body: JSON.stringify(object),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount > 0) {
+          toast.success("Feedback Updated Successfully");
+          window.location.reload();
+        }
+      });
+  };
   return (
     <div>
       <div className="relative">
@@ -81,12 +83,23 @@ const MyReviews = ({service_id}) => {
           </h2>
           <h4 className="lg:text-xl font-semibold text-white">{user?.email}</h4>
         </div>
+      </div>
+      <div className="my-10">
+        {reviews.length > 0 ? (
+          reviews.map((review) => (
+            <MyReviewsRow
+              key={review._id}
+              review={review}
+              handleDelete={handleDelete}
+              handleUpdate={handleUpdate}
+            ></MyReviewsRow>
+          ))
+        ) : (
+          <div className="text-center my-10 text-4xl font-bold border-2 p-5">
+            <h2>NO Reviews or Feedback</h2>
           </div>
-          <div className='my-10'>
-              {
-                  reviews.map(review=><MyReviewsRow key={review._id} review={review} handleDelete={handleDelete} handleUpdate={handleUpdate}></MyReviewsRow>)
-              }
-          </div>
+        )}
+      </div>
     </div>
   );
 };
--- src/pages/ServiceDetails/AllReview/AllReview.js
+++ src/pages/ServiceDetails/AllReview/AllReview.js
@@ -1,27 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import AllReviewRow from './AllReviewRow/AllReviewRow';
+import React, { useEffect, useState } from "react";
+import AllReviewRow from "./AllReviewRow/AllReviewRow";
 
-const AllReview = ({serviceId}) => {
-    const [reviews, setReviews] = useState([])
-    // console.log(reviews);
-    useEffect(() => {
-        fetch(`https://t-drawing-server.vercel.app/reviews?sid=${serviceId}`)
-            .then(res => res.json())
-            .then(data=>setReviews(data));
-    },[])
-    return (
-      <div>
-        <div className="mb-10 mx-5">
-          <p className="text-2xl font-bold text-green-500">Feedback</p>
-          <h2 className="text-5xl font-semibold">Client's Feedback </h2>
-        </div>
-        <div className="overflow-x-auto w-full my-10">
-          {reviews.map((review) => (
-            <AllReviewRow key={review._id} review={review}></AllReviewRow>
-          ))}
-        </div>
+const AllReview = ({ serviceId }) => {
+  const [reviews, setReviews] = useState([]);
+  // console.log(reviews);
+  useEffect(() => {
+    fetch(`https://t-drawing-server.vercel.app/reviews?sid=${serviceId}`)
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, []);
+  return (
+    <div>
+      <div className="mb-10 mx-5">
+        <p className="text-2xl font-bold text-green-500">Feedback</p>
+        <h2 className="text-5xl font-semibold">Client's Feedback </h2>
       </div>
-    );
+      <div className="overflow-x-auto w-full my-10">
+        {reviews.length > 0 ? (
+          reviews.map((review) => (
+            <AllReviewRow key={review._id} review={review}></AllReviewRow>
+          ))
+        ) : (
+          <div className="text-center my-10 text-4xl font-bold border-2 p-5">
+            <h2>NO Reviews or Feedback</h2>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default AllReview;
